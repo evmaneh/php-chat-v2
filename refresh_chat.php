d4c271865beeacae0c7075d6bd0633a957a0d34d
evelyn <122318862+evmaneh@users.noreply.github.com>
2024-02-09 09:47:11 -0500

    Update refresh_chat.php

--- refresh_chat.php
+++ refresh_chat.php
@@ -1,4 +1,5 @@
 <?php
+// Function to get all chat messages
 function getChatMessages() {
     if (file_exists("data/chat_messages.txt")) {
         $messages = file("data/chat_messages.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -8,6 +9,7 @@ function getChatMessages() {
     }
 }
 
+// Get chat messages
 $chatMessages = getChatMessages();
 
 // Output chat messages
